ba20d0fdf93c2375fa864328fd7dffbec6300d91
shubhamkv <shubh082002@gmail.com>
2023-05-22 14:49:19 +0530

    variables
    variables in java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,9 @@ class Hello
 {
     public static void main(String a[])
     {
-        System.out.println("Hello! I am Shubham Kumar");
+        int num1 = 5;
+        int num2 = 3;
+        int result = num1 + num2;
+        System.out.println(result);
     }
 }
