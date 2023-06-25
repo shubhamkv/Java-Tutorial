7605158baea0caef5dd7231a42b9b6b980c7d2f3
shubhamkv <shubh082002@gmail.com>
2023-06-25 15:53:01 +0530

    initial commit

---
+++ array.java
@@ -0,0 +1,13 @@
+public class array
+{
+    public static void main(String args[])
+    {
+        int num[] = new int[4];
+        num[0]=1;
+        num[1]=4;
+        num[2]=6;
+        num[3]=7;
+        for(int i=0;i<4;i++)
+          System.out.println(num[i]);
+    }
+}
